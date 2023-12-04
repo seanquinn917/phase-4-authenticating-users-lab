7c38c3c5123dfe30cea7a9801b3a9b49b796f897
SeanQuinn <Seanquinn917@gmail.com>
2023-12-04 12:09:24 -0500

    commit tree f0fe24242d2cc45dcb6e853da8cbc1d56a89ace6

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+def create
+    user = User.find_by(username: params[:username])
+    session[:user_id] = user.id 
+    render json: user
+end
+
+
+def destroy
+    session[:user_id] = nil
+    render json: { message: "Logged out successfully" }
+    head :no_content
+end 
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:create, :destroy]
+  post '/login', to: 'sessions#create'
+  delete '/logout/', to: 'sessions#destroy'
+
+  resources :users, only: [:show]
+  get '/me', to: 'users#show'
 end
